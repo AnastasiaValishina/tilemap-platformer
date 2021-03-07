675b303bc83c2c84b20906783d969efb0ff56cc8
AnastasiaValishina <azhidenova@gmail.com>
2021-03-07 13:23:45 +0200

    level exit

---
+++ Platformer/Assets/Scripts/LevelExit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    [SerializeField] float waitBeforeNextLevel = 1f;
+    [SerializeField] float slowDownFactor = 0.2f;
+
+    private void OnTriggerEnter2D()
+    {
+        StartCoroutine(LoadNextLevel());
+    }
+
+    IEnumerator LoadNextLevel()
+    {
+        Time.timeScale = slowDownFactor;
+        yield return new WaitForSeconds(waitBeforeNextLevel);
+        Time.timeScale = 1f;
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(++currentSceneIndex);
+    }
+}
