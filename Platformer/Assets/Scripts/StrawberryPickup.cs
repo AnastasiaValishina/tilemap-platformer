7ad74e25e084ac1012d9369d8e6c4c905ca36497
AnastasiaValishina <azhidenova@gmail.com>
2021-03-08 14:19:42 +0200

    collectables added

---
+++ Platformer/Assets/Scripts/StrawberryPickup.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StrawberryPickup : MonoBehaviour
+{
+    [SerializeField] AudioClip pickupSFX;
+    [SerializeField] float timeBeforeDestroying = 1f;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        StartCoroutine(Collect());
+    }
+
+    IEnumerator Collect()
+    {
+        GetComponent<Animator>().SetTrigger("Collected");
+        AudioSource.PlayClipAtPoint(pickupSFX, Camera.main.transform.position);
+        yield return new WaitForSeconds(timeBeforeDestroying);
+        Destroy(gameObject);
+    }
+
+}
