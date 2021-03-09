c4910bbd95ee41519eb4fdd7b779f2f72fdba843
AnastasiaValishina <azhidenova@gmail.com>
2021-03-09 23:37:42 +0200

    water hazard added

---
+++ Platformer/Assets/Scripts/VerticalScroll.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalScroll : MonoBehaviour
+{
+    [SerializeField] float scrollRate = 0.5f;
+    Rigidbody2D rb2d;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        transform.Translate(Vector2.up * scrollRate * Time.deltaTime);
+    }
+}
